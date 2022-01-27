0096df5838f7c36c42b05d674be688b5cef61a62
BlankRip <aniketh_g5@outlook.com>
2022-01-28 03:19:35 +0530

    Only 1 interactable and queing interactables system done

--- Assets/Scripts/Blank/InterationManager.cs
+++ Assets/Scripts/Blank/InterationManager.cs
@@ -8,6 +8,7 @@ namespace Blank {
     {
         private UnityEvent interactEvent;
         private IInteractable currentInteractable;
+        private IInteractable quiedInteractable;
         private bool occupied;
 
         private void Start() {
@@ -16,15 +17,34 @@ namespace Blank {
         }
 
         public void SetInteraction(IInteractable interactable) {
-            if(currentInteractable == null && currentInteractable != interactable) {
+            if(currentInteractable == null) {
                 currentInteractable = interactable;
                 currentInteractable.OnEnter();
+            } else {
+                if(currentInteractable != interactable && quiedInteractable == null) {
+                    quiedInteractable = interactable;
+                }
             }
         }
 
         public void ClearInteration(IInteractable interactable) {
-            if(currentInteractable == interactable)
+            if(quiedInteractable == interactable)
+                quiedInteractable = null;
+            if(currentInteractable == interactable) {
                 currentInteractable = null;
+                if(quiedInteractable != null) {
+                    currentInteractable = quiedInteractable;
+                    StopAllCoroutines();
+                    StartCoroutine(EnterEndOffFrame());
+                    quiedInteractable = null;
+                }
+            }
+        }
+
+        private IEnumerator EnterEndOffFrame() {
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            currentInteractable.OnEnter();
         }
 
         public void Interact() {
