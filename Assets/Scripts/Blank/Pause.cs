296ba5ff6d1a9a8db6d30967507bd7cee3be4d39
BlankRip <aniketh_g5@outlook.com>
2022-01-28 09:11:29 +0530

    Game Pause added

---
+++ Assets/Scripts/Blank/Pause.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Blank {
+    public class Pause : MonoBehaviour
+    {
+        [SerializeField] GameObject pausePanel, warningPanel;
+        [SerializeField] Button resume, restart, backToMenu, warningYes, warningNo;
+        private bool paused;
+
+        private void Start() {
+            resume.onClick.AddListener(ResumeGame);
+            restart.onClick.AddListener(() => {Time.timeScale = 1; SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);});
+            backToMenu.onClick.AddListener(() => {warningPanel.SetActive(true);});
+            warningNo.onClick.AddListener(() => {warningPanel.SetActive(false);});
+            warningYes.onClick.AddListener(() => {Time.timeScale = 1; SceneManager.LoadScene(0);});
+        }
+
+        private void Update() {
+            if(Input.GetKeyDown(KeyCode.Escape)) {
+                if(!paused)
+                    PauseGame();
+                else
+                    ResumeGame();
+            }
+        }
+
+        private void PauseGame() {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            paused = true;
+        } 
+
+        private void ResumeGame() {
+            Time.timeScale = 1;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            pausePanel.SetActive(false);
+            paused = false;
+        }
+    }
+}
